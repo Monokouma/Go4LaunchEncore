623a829b14b0c698efb2ce4085fd52241c330927
Nino <anthony@delcey.fr>
2023-10-18 20:58:31 +0200

    Fix SharedElementTransitions

--- ui/src/main/java/com/despaircorp/ui/main/bottom_bar/BottomBarActivity.kt
+++ ui/src/main/java/com/despaircorp/ui/main/bottom_bar/BottomBarActivity.kt
@@ -13,6 +13,8 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.toAndroidXPair
+import androidx.core.view.postDelayed
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.despaircorp.ui.R
@@ -34,126 +36,128 @@ class BottomBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        
+
         val headerBinding = HeaderNavigationDrawerBinding.bind(
             binding.activityBottomBarNavigationViewProfile.getHeaderView(0)
         )
-        
+
         val fade = Fade().apply {
             duration = 2000
         }
         window.enterTransition = fade
-        
+
         setSupportActionBar(binding.activityBottomBarToolbarRoot)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        
+
         getLocationPermission()
-        
+
         loadFragment(MapFragment())
-        
+
         binding.activityBottomBarBottomBar.selectedItemId =
             savedInstanceState?.getInt(KEY_BOTTOM_NAV_BAR_SELECTED_ITEM_ID, R.id.navigation_map)
                 ?: R.id.navigation_map
-        
+
         binding.activityBottomBarToolbarRoot.setNavigationOnClickListener {
             binding.activityBottomBarDrawerLayout.open()
         }
-        
+
         binding.activityBottomBarBottomBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_restaurants -> {
                     loadFragment(RestaurantsFragment())
                     true
                 }
-                
+
                 R.id.navigation_workmates -> {
                     loadFragment(CoworkersFragment())
                     true
                 }
-                
+
                 else -> {
                     loadFragment(MapFragment())
                     true
                 }
             }
         }
-        
+
         binding.activityBottomBarNavigationViewProfile.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.your_lunch -> Log.i("Monokouma", "your lunch")
+                R.id.your_lunch -> {
+                    Log.i("Monokouma", "your lunch")
+                    binding.activityBottomBarDrawerLayout.close()
+                }
                 R.id.settings -> {
-                    val intent = UserSettingsActivity.navigate(this).apply {
-                        putExtra(
-                            binding.activityBottomBarImageViewLogo.transitionName,
-                            binding.activityBottomBarImageViewLogo.transitionName
-                        )
-                        putExtra(
-                            headerBinding.navigationDrawerImageViewUserImage.transitionName,
-                            headerBinding.navigationDrawerImageViewUserImage.transitionName
-                        )
-                        putExtra(
-                            headerBinding.navigationDrawerTextViewUserName.transitionName,
-                            headerBinding.navigationDrawerTextViewUserName.transitionName
-                        )
-                        putExtra(
-                            headerBinding.navigationDrawerTextViewUserMail.transitionName,
-                            headerBinding.navigationDrawerTextViewUserMail.transitionName
-                        )
-                        
-                    }
-                    
+                    val intent = UserSettingsActivity.navigate(this)
+
                     startActivity(
                         intent,
-                        ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle()
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(
+                            this,
+                            Pair(
+                                headerBinding.navigationDrawerTextViewUserName,
+                                headerBinding.navigationDrawerTextViewUserName.transitionName
+                            ).toAndroidXPair(),
+                            Pair(
+                                headerBinding.navigationDrawerTextViewUserMail,
+                                headerBinding.navigationDrawerTextViewUserMail.transitionName
+                            ).toAndroidXPair(),
+                            Pair(
+                                headerBinding.navigationDrawerImageViewUserImage,
+                                headerBinding.navigationDrawerImageViewUserImage.transitionName
+                            ).toAndroidXPair()
+                        ).toBundle()
                     )
-                    
+                    binding.activityBottomBarNavigationViewProfile.postDelayed(500) {
+                        binding.activityBottomBarDrawerLayout.close()
+                    }
                 }
-                
+
                 R.id.logout -> {
                     viewModel.onDisconnectUser()
+                    binding.activityBottomBarDrawerLayout.close()
                 }
             }
-            binding.activityBottomBarDrawerLayout.close()
+
             true
         }
-        
-        
-        
+
+
+
         viewModel.viewState.observe(this) {
             Glide.with(headerBinding.navigationDrawerImageViewUserImage).load(it.userImage)
                 .into(headerBinding.navigationDrawerImageViewUserImage)
             headerBinding.navigationDrawerTextViewUserMail.text = it.emailAddress
             headerBinding.navigationDrawerTextViewUserName.text = it.username
         }
-        
+
         viewModel.viewAction.observe(this) {
             when (val action = it.getContentIfNotHandled()) {
-                
+
                 is BottomBarAction.Error -> Toast.makeText(
                     this,
                     getString(action.message),
                     Toast.LENGTH_SHORT
                 ).show()
-                
+
                 BottomBarAction.OnDisconnect -> {
                     startActivity(LoginActivity.navigate(this))
                     finishAffinity()
                 }
-                
+
                 else -> Unit
             }
         }
     }
-    
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        
+
         outState.putInt(
             KEY_BOTTOM_NAV_BAR_SELECTED_ITEM_ID,
             binding.activityBottomBarBottomBar.selectedItemId
         )
     }
-    
+
     @SuppressLint("CommitTransaction")
     private fun loadFragment(fragment: Fragment) {
         val previousFragment = supportFragmentManager.findFragmentByTag(fragment.javaClass.name)
@@ -164,7 +168,7 @@ class BottomBarActivity : AppCompatActivity() {
                 .commit()
         }
     }
-    
+
     private fun getLocationPermission() {
         if (ActivityCompat.checkSelfPermission(
                 this,
@@ -182,11 +186,11 @@ class BottomBarActivity : AppCompatActivity() {
             return
         }
     }
-    
+
     companion object {
         private const val KEY_BOTTOM_NAV_BAR_SELECTED_ITEM_ID =
             "KEY_BOTTOM_NAV_BAR_SELECTED_ITEM_ID"
-        
+
         fun navigate(context: Context) = Intent(
             context,
             BottomBarActivity::class.java
--- ui/src/main/java/com/despaircorp/ui/main/settings/UserSettingsActivity.kt
+++ ui/src/main/java/com/despaircorp/ui/main/settings/UserSettingsActivity.kt
@@ -2,11 +2,20 @@ package com.despaircorp.ui.main.settings
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AlphaAnimation
+import android.view.animation.LinearInterpolator
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.transition.addListener
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.despaircorp.ui.R
 import com.despaircorp.ui.databinding.ActivityUserSettingsBinding
 import com.despaircorp.ui.utils.viewBinding
@@ -21,67 +30,87 @@ class UserSettingsActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.activityUserSettingsToolbarRoot)
         supportActionBar?.setDisplayShowTitleEnabled(false)
-        
+
+        postponeEnterTransition()
+
+
+
         binding.activityUserSettingsToolbarRoot.setNavigationOnClickListener {
             finish()
         }
-        
-        
-        supportPostponeEnterTransition()
-        
-        binding.activityUserSettingsImageViewLogo.viewTreeObserver.addOnPreDrawListener {
-            startPostponedEnterTransition()
-            true
-        }
-        binding.activityUserSettingsImageViewUserImage.viewTreeObserver.addOnPreDrawListener {
-            startPostponedEnterTransition()
-            true
-        }
-        binding.activityUserSettingsTextViewDisplayName.viewTreeObserver.addOnPreDrawListener {
-            startPostponedEnterTransition()
-            true
-        }
-        binding.activityUserSettingsTextViewEmail.viewTreeObserver.addOnPreDrawListener {
-            startPostponedEnterTransition()
-            true
-        }
-        
+
         binding.activityUserSettingsSwitchNotif.setOnCheckedChangeListener { _, b ->
             viewModel.onSwitchChanged(b)
         }
-        
+
         binding.activityUserSettingsTextViewDisplayName.setOnClickListener {
-            
+
         }
-        
+
         viewModel.viewState.observe(this) {
-            Glide.with(binding.activityUserSettingsImageViewUserImage).load(it.userImageUrl)
+            Glide.with(binding.activityUserSettingsImageViewUserImage)
+                .load(it.userImageUrl)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        startPostponedEnterTransition()
+
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        startPostponedEnterTransition()
+
+                        return false
+                    }
+                })
                 .into(binding.activityUserSettingsImageViewUserImage)
             binding.activityUserSettingsTextViewDisplayName.text = it.displayName
             binding.activityUserSettingsTextViewEmail.text = it.emailAddress
             binding.activityUserSettingsSwitchNotif.isChecked = it.isNotificationsChecked
         }
-        
+
         viewModel.viewAction.observe(this) {
             when (val action = it.getContentIfNotHandled()) {
-                
+
                 is UserSettingsAction.Error -> Toast.makeText(
                     this,
                     getString(action.message),
                     Toast.LENGTH_SHORT
                 ).show()
-                
+
                 UserSettingsAction.ModificationsSuccess -> Toast.makeText(
                     this,
                     getString(R.string.preferences_saved),
                     Toast.LENGTH_SHORT
                 ).show()
-                
+
                 else -> Unit
             }
         }
+
+        window.sharedElementEnterTransition.addListener(
+            onEnd = {
+                binding.activityUserSettingsImageViewUserImageEdit.startAnimation(
+                    AlphaAnimation(0F, 0.7F).apply {
+                        interpolator = LinearInterpolator()
+                        duration = 300
+                    }
+                )
+            }
+        )
     }
-    
+
     companion object {
         fun navigate(context: Context) = Intent(
             context,
